b54fe54af6b928709068227bdecb2f101cf52f46
happybirthdayananya <118069221+happybirthdayananya@users.noreply.github.com>
2022-11-12 22:46:42 +0530

    Update audio.js

--- audio.js
+++ audio.js
@@ -3,7 +3,7 @@ window.alert("Headphone laga lena sun ne ke pehle, There is something I wana ded
 
 $(function()
 {
-    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false, albums = ['Thank You'], trackNames = ['Happy Birthday Priya'], albumArtworks = ['_1'], trackUrl = ['2.mp3'], currIndex = -1;
+    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false, albums = ['I love you'], trackNames = ['Happy Birthday Anuu'], albumArtworks = ['_1'], trackUrl = ['2.mp3'], currIndex = -1;
 
     function playPause()
     {
